255065a08168b9dd4d63ba8962a1e310335f277b
Meghana2399778 <meghanaboddu23@gmail.com>
2026-09-09 07:37:46 +0530

    Update DetectCycleUndirected.java

--- Graphs/Cycle Detection/DetectCycleUndirected.java
+++ Graphs/Cycle Detection/DetectCycleUndirected.java
@@ -52,3 +52,50 @@ class Solution {
         
     }
 }
+
+
+DFS:
+
+class Tuple{
+    int node,parent;
+    Tuple(int node,int parent){
+        this.node=node;
+        this.parent=parent;
+    }
+}
+class Solution {
+    private boolean dfs(int node,int parent,boolean visit[],List<List<Integer>> adj){
+        visit[node]=true;
+        for(int num:adj.get(node)){
+            if(visit[num]!=true){
+                if(dfs(num,node,visit,adj)) return true;
+            }
+            else if(num!=parent){
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean isCycle(int V, int[][] edges) {
+        // Code here
+        List<List<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<V;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int edge[]:edges){
+            int u=edge[0];
+            int v=edge[1];
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+        boolean visit[]=new boolean[V];
+        for(int i=0;i<V;i++){
+            if(!visit[i]){
+                if(dfs(i,-1,visit,adj)) return true;
+            }
+        }
+        
+        return false;
+        
+    }
+}
