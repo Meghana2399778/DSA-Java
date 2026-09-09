0c5480fbac6e65a2041705b7f47efb94b55f56b2
Meghana2399778 <meghanaboddu23@gmail.com>
2026-09-09 07:33:59 +0530

    Create DetectCycleUndirected.java

---
+++ Graphs/Cycle Detection/DetectCycleUndirected.java
@@ -0,0 +1,54 @@
+BFS:
+
+class Tuple{
+    int node,parent;
+    Tuple(int node,int parent){
+        this.node=node;
+        this.parent=parent;
+    }
+}
+class Solution {
+    private boolean bfs(int start,boolean visit[],List<List<Integer>> adj){
+        Queue<Tuple> que=new LinkedList<>();
+        que.offer(new Tuple(start,-1));
+        visit[start]=true;
+        
+        while(!que.isEmpty()){
+            Tuple tup=que.poll();
+            int node=tup.node;
+            int par=tup.parent;
+        for(int num:adj.get(node)){    
+            if(visit[num]!=true){
+                visit[num]=true;
+                que.offer(new Tuple(num,node));
+            }
+            else if(num!=par){
+                return true;
+            }
+        }
+        }
+        return false;
+    }
+    public boolean isCycle(int V, int[][] edges) {
+        // Code here
+        List<List<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<V;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int edge[]:edges){
+            int u=edge[0];
+            int v=edge[1];
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+        boolean visit[]=new boolean[V];
+        for(int i=0;i<V;i++){
+            if(!visit[i]){
+                if(bfs(i,visit,adj)) return true;
+            }
+        }
+        
+        return false;
+        
+    }
+}
